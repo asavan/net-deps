e42d13058ac0c16648d3b0a58c4c032216ac1a27
a.savanovich <asavanster@gmail.com>
2025-10-06 15:01:34 +0300

    update devtools

--- src/devtools/ws-server.js
+++ src/devtools/ws-server.js
@@ -1,27 +1,26 @@
 import { WebSocketServer } from "ws";
 
+const onConnection = (ws, req) => {
+    // https://stackoverflow.com/questions/14822708/how-to-get-client-ip-address-with-websocket-websockets-ws-library-in-node-js
+    console.log("WS connection established!", req.socket.remoteAddress, req.headers['sec-websocket-key']);
+
+    ws.on("close", function () {
+        console.log("WS closed!");
+    });
+
+    ws.on("message", function (message) {
+        console.log("Got ws message: " + message);
+        for (const candidate of wss.clients) {
+            // send to everybody on the site, except sender
+            if (candidate !== ws && candidate.readyState === WebSocket.OPEN) {
+                // console.log("Send ws message: " + message);
+                candidate.send(message);
+            }
+        }
+    });
+};
+
 export default function wsServer(port) {
     const wss = new WebSocketServer({port: port});
-    const wsList = [];
-
-    wss.on("connection", (ws, req) => {
-        // https://stackoverflow.com/questions/14822708/how-to-get-client-ip-address-with-websocket-websockets-ws-library-in-node-js
-        console.log("WS connection established!", req.socket.remoteAddress);
-        wsList.push(ws);
-
-        ws.on("close", function () {
-            wsList.splice(wsList.indexOf(ws), 1);
-            console.log("WS closed!");
-        });
-
-        ws.on("message", function (message) {
-            console.log("Got ws message: " + message);
-            for (const candidate of wsList) {
-                // send to everybody on the site, except sender
-                if (candidate !== ws) {
-                    candidate.send(message);
-                }
-            }
-        });
-    });
+    wss.on("connection", onConnection);
 }
