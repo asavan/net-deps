eba4506649addaff7dc8546bf617121901b295f3
a.savanovich <asavanster@gmail.com>
2025-10-06 18:55:38 +0300

    stylelint as js config

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ export { default as webpackDev } from "./src/webpack_configs/webpack.config.js";
 export { default as webpackProd } from "./src/webpack_configs/production.webpack.config.js";
 export { default as webpackAndroid } from "./src/webpack_configs/android.webpack.config.js";
 export { default as esLintConf } from "./src/linters/eslint.config.js";
+export { default as stylelintConf } from "./src/linters/stylelint.config.js";
