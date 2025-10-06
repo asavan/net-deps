b4308273839f8d696d16c4d8453c2a7eec3ec092
a.savanovich <asavanster@gmail.com>
2025-10-06 15:13:22 +0300

    export configs

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 export { default as wsServer } from "./src/devtools/ws-server.js";
+export { default as webpackDev } from "./src/webpack_configs/webpack.config.js";
+export { default as webpackProd } from "./src/webpack_configs/production.webpack.config.js";
+export { default as webpackAndroid } from "./src/webpack_configs/android.webpack.config.js";
+export { default as esLintConf } from "./src/linters/eslint.config.js";
